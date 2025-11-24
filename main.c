69d93a324e165b8d155e0babc7137663d34a07c3
tomproux <77828137+tomproux@users.noreply.github.com>
2025-11-24 14:31:02 +0100

    main.c

--- main.c
+++ main.c
@@ -21,18 +21,25 @@ int main(void)
 
 	_printf("Length:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);
+	
 	_printf("Negative:[%d]\n", -762534);
 	printf("Negative:[%d]\n", -762534);
+	
 	_printf("Unsigned:[%u]\n", ui);
 	printf("Unsigned:[%u]\n", ui);
+	
 	_printf("Unsigned octal:[%o]\n", ui);
 	printf("Unsigned octal:[%o]\n", ui);
+	
 	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
 	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+	
 	_printf("Character:[%c]\n", 'H');
 	printf("Character:[%c]\n", 'H');
+	
 	_printf("String:[%s]\n", "I am a string !");
 	printf("String:[%s]\n", "I am a string !");
+	
 	_printf("Address:[%p]\n", addr);
 	printf("Address:[%p]\n", addr);
 
@@ -41,6 +48,7 @@ int main(void)
 
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
+	
 	_printf("Unknown:[%r]\n");
 	printf("Unknown:[%r]\n");
 
