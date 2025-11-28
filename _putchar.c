cffd846cf6968e4bee34c376f76b267e1b503d33
tomproux <tomproux53@gmail.com>
2025-11-28 17:58:19 +0000

    _putchar

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar - writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1, on error -1
+ */
+
+char _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
