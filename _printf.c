e974ea835e7132a82bcc070987c4262fe55a795d
tomproux <tomproux53@gmail.com>
2025-11-28 14:48:39 +0000

    Fonction _printf

--- _printf.c
+++ _printf.c
@@ -5,8 +5,8 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count = 0, j = 0, d, i;
-	char c, less = '-';
+	int count = 0, j = 0; /*d, i;*/
+	char c; /*less = '-';*/
 	char *str;
 
 	if (!format)
@@ -19,52 +19,44 @@ int _printf(const char *format, ...)
 		if (format[j] == '%')
 		{
 			j++;
-			if (format[j] == '\0')
+			if (format[j] == 'c')
 			{
-				write(1, "\0", 1);
+				c = (char)va_arg(args, int);
+				write(1, &c, 1);
 				count++;
+			}
+			else if (format[j] == 's')
+			{
+				str = va_arg(args, char *);
+				if (!str)
+					str = "(null)";
+				while (*str)
+				{
+					write(1, str, 1);
+					str++;
+					count++;
+				}
+			}
+			/*else if (format[j] == 'd')
+			{
+				d = va_arg(args, int);
+				print_digit(d);
+			else if (format[j] == 'i')
+			{
+				i = va_arg(args, int);
+				print_digit(i);
+			}*/
+			else if (format[j] == '%')
+			{
+				write(1, "%", 1);
+				count ++;
 			}
 			else
 			{
-				if (format[j] == 'c')
-				{
-					c = (char)va_arg(args, int);
-					write(1, &c, 1);
-					count++;
-				}
-				else if (format[j] == 's')
-				{
-					str = va_arg(args, char *);
-					if (!str)
-						str = "(null)";
-					while (*str)
-					{
-						write(1, str, 1);
-						str++;
-						count++;
-					}
-				}
-				else if (format[j] == 'd')
-				{
-					d = va_arg(args, int);
-					print_digit(d);
-				else if (format[j] == 'i')
-				{
-					i = va_arg(args, int);
-					print_digit(i);
-				}
-				else if (format[j] == '%')
-				{
-					write(1, "%", 1);
-					count ++;
-				}
-				else
-				{
-					/* unknow character */
-					write(1, "%", 1);
-					write(1, &format[j], 1);
-					count += 2;
-				}
+				/* unknow character */
+				write(1, "%", 1);
+				write(1, &format[j], 1);
+				count += 2;
 			}
 		}
 		else
