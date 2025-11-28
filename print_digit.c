ca97d2d505f93e43f6876230a332c142dd355a85
tomproux <tomproux53@gmail.com>
2025-11-28 17:59:00 +0000

    Fonction %d et %i

--- print_digit.c
+++ print_digit.c
@@ -1,27 +1,27 @@
 #include "main.h"
 #include <stdarg.h>
-#include <stdio.h>
 
-void print_digit(int number)
+int print_di(va_list args)
 {
-	number = 0;
-
-	printf("%d", number);
-/*	int modulo = 10;
-	va_list args;
-
-	number = va_args(args, int);
+	int count = 0;
+	long number = va_arg(args, int);
+	long modulo = 10;
+	long print_num, cp_num;
 
 	if (number < 0)
 	{
-		write(1, "-", 1);
-		write(1, &number, 1);
+		_putchar('-');
+		number *= -1;
+		count++;
 	}
-	else if (number >= 0 && number < modulo)
+	if (number >= 0 && number <= 9)
 	{
-		write(1, &number, 1);
+		count += _putchar(number + '0');
 	}
-	else
+
+	cp_num = number;
+
+	if (number > 9)
 	{
 		while (number >= (modulo * 10))
 		{
@@ -29,9 +29,11 @@ void print_digit(int number)
 		}
 		while (modulo >= 1)
 		{
-			write(1, &number, 1);
+			print_num = cp_num / modulo;
+			count += _putchar(print_num + '0');
+			cp_num %= modulo;
 			modulo /= 10;
 		}
-		write(1, &number, 1);
-	}*/
+	}
+	return (count);
 }
