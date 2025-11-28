51530ec798ea1361f9bd397701fcc2ce1af2aa97
tomproux <tomproux53@gmail.com>
2025-11-28 13:05:31 +0000

    Fonction _printf

--- _printf.c
+++ _printf.c
@@ -19,70 +19,73 @@ int _printf(const char *format, ...)
 		if (format[j] == '%')
 		{
 			j++;
-			if (format[j] == 'c')
-			{
-				c = (char)va_arg(args, int);
-				write(1, &c, 1);
-				count++;
-			}
-			else if (format[j] == 's')
-			{
-				str = va_arg(args, char *);
-				if (!str)
-					str = "(null)";
-				while (*str)
-				{
-					write(1, str, 1);
-					str++;
-					count++;
-				}
-			}
-			else if (format[j] == 'd')
-			{
-				d = va_arg(args, int);
-				if (d < 0)
-				{
-					write(1, &less, 1);
-					write(1, &d, 1);
-					count++;
-				}
-				else
-				{
-					write(1, &d, 1);
-					count++;
-				}
-			}
-			else if (format[j] == 'i')
-			{
-				i = va_arg(args, int);
-				if (i < 0)
-				{
-					write(1, &less, 1);
-					write(1, &i, 1);
-					count++;
-				}
-				else
-				{
-					write(1, &i, 1);
-					count++;
-				}
-			}
-			else if (format[j] == '\0')
+			if (format[j] == '\0')
 			{
 				write(1, "\0", 1);
 				count++;
 			}
-			else if (format[j] == '%')
-			{
-				write(1, "%", 1);
-				count ++;
-			}
 			else
 			{
-				/* unknow character */
-				write(1, "%", 1);
-				write(1, &format[j], 1);
-				count += 2;
+				if (format[j] == 'c')
+				{
+					c = (char)va_arg(args, int);
+					write(1, &c, 1);
+					count++;
+				}
+				else if (format[j] == 's')
+				{
+					str = va_arg(args, char *);
+					if (!str)
+						str = "(null)";
+					while (*str)
+					{
+						write(1, str, 1);
+						str++;
+						count++;
+					}
+				}
+				else if (format[j] == 'd')
+				{
+					d = va_arg(args, int);
+					if (d < 0)
+					{
+						write(1, &less, 1);
+						write(1, &d, 1);
+						count++;
+					}
+					else
+					{
+						write(1, &d, 1);
+						count++;
+					}
+				}
+				else if (format[j] == 'i')
+				{
+					i = va_arg(args, int);
+					if (i < 0)
+					{
+						write(1, &less, 1);
+						write(1, &i, 1);
+						count++;
+					}
+					else
+					{
+						write(1, &i, 1);
+						count++;
+					}
+				}
+				else if (format[j] == '%')
+				{
+					write(1, "%", 1);
+					count ++;
+				}
+				else
+				{
+					/* unknow character */
+					write(1, "%", 1);
+					write(1, &format[j], 1);
+					count += 2;
+				}
 			}
 		}
 		else
